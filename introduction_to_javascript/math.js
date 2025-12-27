9f72362f8eb7abf0b3ec73c34ebfa1e0d48bda0e
alshahedbishwas <shahedevise@gmail.com>
2025-12-28 00:23:33 +0600

    introduction to javascript and math nedded for programming

---
+++ introduction_to_javascript/math.js
@@ -0,0 +1,15 @@
+/**
+ * math nedded for programming
+ *
+ * 1. number: (+ve) positive, (-ve) negative, 0 [ 50, -9, 0 ]
+ * 2. integer: 2, 88, [odd, even]: 2, 4, 897
+ * 3. float: 5.6, 2.2
+ * 4. operation: +, -, *, /
+ * 5. more operation: %, [ ]
+ * 6. percentage: 5%, 50 taka er 10% koto?
+ * 7. sum: 14, 18, 16
+ * 8. avg: 14, 18, 16
+ * 9. max/min: 14, 18, 16
+ * 10. series:
+ * 11. area: triangle
+ */
