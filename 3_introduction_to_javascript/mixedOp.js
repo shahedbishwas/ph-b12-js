0a5e5fa35293bc570c58c019faf5aab49492d829
alshahedbishwas <shahedevise@gmail.com>
2025-12-28 18:31:18 +0600

    13-10 (advanced) Mathematical operation shorthand

---
+++ 3_introduction_to_javascript/mixedOp.js
@@ -0,0 +1,9 @@
+var first = "AL";
+var second = "Shahed";
+
+var fullName = first + second;
+console.log(fullName);
+
+console.log(40 + "3");
+console.log(30 - "20");
+console.log("50" - 10);
---
+++ 3_introduction_to_javascript/shorthand.js
@@ -0,0 +1,20 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+// console.log(price);
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000;
+// console.log(fridge);
+
+// multiply
+var orangePrice = 30;
+// orangePrice = orangePrice * 3;
+orangePrice *= 3;
+// console.log(orangePrice);
+
+var mangoPrice = 60;
+// mangoPrice = mangoPrice / 2;
+mangoPrice /= 2;
+console.log(mangoPrice);
