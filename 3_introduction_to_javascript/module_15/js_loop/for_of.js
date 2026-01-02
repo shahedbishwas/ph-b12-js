707ae4f91c4943f9b227440d3d10d65afa217c31
alshahedbishwas <shahedevise@gmail.com>
2026-01-02 23:15:49 +0600

    15_1 What is a Loop, explore for loop

---
+++ 3_introduction_to_javascript/module_15/js_loop/for_of.js
@@ -0,0 +1,14 @@
+const numbers = [15, 12, 54, 85, 65];
+
+console.log(numbers);
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+// onno arekta
+const fruits = ["apple", "banana", "angur", "mango", "watermelone"];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
