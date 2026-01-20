1bf64742d4b6578d2fb98393671fe7c40da1efec
codebyshahed <shahedevise@gmail.com>
2026-01-21 04:57:30 +0600

    feat: module 17.5.... js simple function tasks gula complete kore felsi. jog, biyog, gun ar vag korar machine banaite koichilo

---
+++ milestone_3_introduction_to_javascript/module_17.5/simple_function_tasks/simple_function_tasks.js
@@ -0,0 +1,37 @@
+/**
+ * jog korar machine
+ */
+function add(num1, num2) {
+  let sum = num1 + num2;
+  console.log(`Sum is: ${sum}`);
+}
+
+add(100, 20);
+
+/**
+ * biyog korar machine
+ */
+function subsTraction(num1, num2) {
+  let diff = num1 - num2;
+  console.log(`Diff is: ${diff}`);
+}
+
+subsTraction(100, 40);
+
+/**
+ * gun korar machine
+ */
+function multiplication(num1, num2) {
+  let mult = num1 * num2;
+  console.log(`Mult is: ${mult}`);
+}
+multiplication(10, 2);
+
+/**
+ * vag korar machine
+ */
+function division(num1, num2) {
+  let divide = num1 / num2;
+  console.log(`Division is: ${divide}`);
+}
+division(50, 2);
