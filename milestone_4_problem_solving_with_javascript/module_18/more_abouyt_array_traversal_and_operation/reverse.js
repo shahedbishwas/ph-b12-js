053758355f8eb6d6fed653ffe25f83f105f2e34f
codebyshahed <shahedevise@gmail.com>
2026-01-24 18:02:03 +0600

    feat: ager video ta khub valovabe dekhlam and reverse array jinista bujlam and practice korlam

--- milestone_4_problem_solving_with_javascript/module_18/more_abouyt_array_traversal_and_operation/reverse.js
+++ milestone_4_problem_solving_with_javascript/module_18/more_abouyt_array_traversal_and_operation/reverse.js
@@ -1,30 +1,38 @@
-const numbers = [2, 6, 8, 4, 96, 7];
-let reversed = [];
-
-for (let i = 0; i < numbers.length; i++) {
-  // let number = numbers[i];
-  // reversed.unshift(number);
-}
-// console.log(reversed);
+const numbers = [1, 2, 3, 4, 5];
+let reversedArray = [];
 
 /**
- * using olta method
+ * for loop use kore array er protita element pacchi
+ *
+ * unshift use kore eikhne array protekta upadan ke first e boshacchi
+ */
+for (let i = 0; i < numbers.length; i++) {
+  // reversedArray.unshift(numbers[i]);
+}
+// console.log(reversedArray);
+
+/**
+ * ekhn array take ekdom ultavabe lopp korbo and also push methode use korbo
  */
 for (let i = numbers.length - 1; i >= 0; i--) {
-  // reversed.push(numbers[i]);
+  // reversedArray.push(numbers[i]);
 }
-// console.log(reversed);
+// console.log(reversedArray);
 
 /**
- * using best practice
+ * loop use kore array ke reverse korte chaile for of is the best choise and shortcut
  */
-for (const num of numbers) {
-  // reversed.unshift(num);
+for (const number of numbers) {
+  // reversedArray.unshift(number);
 }
-// console.log(reversed);
+// console.log(reversedArray);
 
 /**
- * using reverse() method
+ * reverse() methode use kore. eita rray er built in methode and eita aro shohoj and simple
+ *
+ * ei methode ta array takei change kore dei. mane in place sei full array er moddhei change kore fele. tobe chaile amra eitake ekta variable er mode rakhte pari. but amra change korle ager array take pabona.. in place cng kore dei
+ *
+ * ei method array take sei array er moddhei cng kore fele. mae eita main array take cng kore dei, ami r original array take pabo na
  */
 console.log(numbers);
 numbers.reverse();
