c017a30a3dccb82f080b1f400ac4249ba614220d
codebyshahed <shahedevise@gmail.com>
2026-01-28 17:12:12 +0600

    feat: 19-5 Function return and set return value to a variable

---
+++ milestone_4_problem_solving_with_javascript/module_19/js_function/retutn.js
@@ -0,0 +1,18 @@
+/**
+ * function number er sathe 10 gun
+ */
+function tenTimes(num) {
+  const result = num * 10;
+  return result;
+}
+
+/**
+ * function curHalf. mane jei number dimu seitake tumi ordhek kore diba
+ */
+function cutHalf(num) {
+  const result = num / 2;
+  return result;
+}
+
+const output = tenTimes(2);
+console.log(`output: ${output}`);
